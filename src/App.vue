8af2dccb6343908c1e2d714922fe7c6b9da09845
pab-ns <84049748+pab-ns@users.noreply.github.com>
2021-10-10 09:23:58 -0300

    subir desafio03

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,83 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <!-- nav -->
+    <v-app-bar dense class="red lighten-1">
+      <v-toolbar-title class="white--text"
+        >Rick & Morty Personajes</v-toolbar-title
+      >
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <!-- content 3 -->
+    <v-container>
+      <v-row no-gutters>
+        <v-col
+          v-for="(personaje, $index) in personajes"
+          :key="$index"
+          cols="12"
+          sm="4"
+        >
+          <v-card class="pa-2" outlined tile>
+            <v-img :src="personaje.imagen"></v-img>
+            <v-card-title>{{ personaje.nombre }}</v-card-title>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <!-- content 1 -->
+    <!-- content 2 -->
+
+    <!-- footer -->
+    <v-footer padless>
+      <v-card flat tile width="100%" class="red lighten-1 text-center">
+        <v-divider></v-divider>
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>Rick & Morty</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: () => ({
+    personajes: [
+      {
+        id: 1,
+        nombre: "Morty",
+        imagen: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+        flex: 6,
+      },
+      {
+        id: 2,
+        nombre: "Rick",
+        imagen: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+        flex: 6,
+      },
+      {
+        id: 3,
+        nombre: "Summer",
+        imagen: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+        flex: 6,
+      },
+      {
+        id: 4,
+        nombre: "Beth",
+        imagen: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+        flex: 6,
+      },
+      {
+        id: 5,
+        nombre: "Jerry",
+        imagen: "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+        flex: 6,
+      },
+    ],
+  }),
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/test.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!--
+        Test 1  / Content 1
+        <v-card class="mt-5" tile v-for="(personaje, $index) in personajes" :key="$index">
+            <v-img :src="personaje.imagen"></v-img>
+            <v-card-title>{{ personaje.nombre }}</v-card-title>
+        </v-card>
+
+        Test 2  / Content 2
+        <v-container fluid>
+            <v-row dense>
+                <v-col v-for="personaje in personajes" :key="personaje.nombre" :col="personaje.flex">
+                <v-card>
+                    <v-img :src="personaje.imagen"> </v-img>
+                    <v-card-title v-text="personaje.nombre"></v-card-title>
+                </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    -->
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
